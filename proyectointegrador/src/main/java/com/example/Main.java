4253578e721cd28b261295e0811f763361e695af
Spuerta18 <santiagopuerta1996@gmail.com>
2024-05-28 20:51:05 -0500

    cambio2

--- proyectointegrador/src/main/java/com/example/Main.java
+++ proyectointegrador/src/main/java/com/example/Main.java
@@ -1,7 +1,48 @@
 package com.example;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bienvenido");
+        System.out.print(" Aqui te cotizaremos lo necesario para tu proyecto solar");
+        System.out.print(" Ingresa tu nombre y apellido");
+        System.out.print(" Ingresa tu costo por kwh");
+        string nombre = sc.nextLine();
+
+
+
+
+        // System.out.println("Ingrese el consumo de kWh");
+        // double consumokWh = sc.nextDouble();
+        // System.out.println("Ingrese el valor por kWh");
+        // double valorkWh = sc.nextDouble();
+        // double subtotal = consumokWh * valorkWh;
+        // double iva = subtotal * 0.19;
+        // double alumbradopublico = 5000;
+        // double totalfactura = subtotal + iva + alumbradopublico;
+        // System.out.println("El consumo en Kwh es: " + consumokWh);
+        // System.out.println("El valor en Kwh es: " + valorkWh);
+        // System.out.println("El subtotal de la factura es: $" + subtotal);
+        // System.out.println("El IVA de la factura es: $" + iva);
+        // System.out.println("El total de la factura es: $" + totalfactura);
+    
+        sc.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
